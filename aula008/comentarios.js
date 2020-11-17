324c9c55ac0844a61c86ce9b6f1d57a0d0d025b0
LeonardoMendoncaInfoglobo <leonardo.mendonca@infoglobo.com.br>
2020-11-16 21:33:31 -0300

    Comentarios

---
+++ aula008/comentarios.js
@@ -0,0 +1,16 @@
+// Escrever comentário de linha
+console.log('Olá mundo') // Aqui tem outro comentário
+console.log('Linha 1')
+console.log('Linha 2')
+console.log('Linha 3')
+console.log('Linha 4')
+// Vai ser ignorado
+
+/**
+ * Escrever comentário de bloco
+ */
+
+/*
+Outro comentário de bloco
+*/
+
